9f462e8f3de29c1789d3d404e7a10be3fd63754b
Cesar Garcia <cesar.garcia.satx@gmail.com>
2026-04-17 16:01:11 +0000

    Lab #1

--- run_exercises.py
+++ run_exercises.py
@@ -1,5 +1,6 @@
-'''python
+import portfolio.data
+import portfolio.report
+
 if __name__ == "__main__":
     my_portfolio = portfolio.data.create_portfolio("Retirement")
     portfolio.report.print_report(my_portfolio)
-'''
